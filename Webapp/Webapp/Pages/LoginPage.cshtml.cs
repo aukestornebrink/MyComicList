5eb882fa0b4f1617d9b76d6b1ef06b329f6127d9
aukestornebrink <auke.stornebrink@student.nhlstenden.com>
2022-03-18 14:15:47 +0100

    fixed button

--- Webapp/Webapp/Pages/LoginPage.cshtml.cs
+++ Webapp/Webapp/Pages/LoginPage.cshtml.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc.RazorPages;
 using MySql.Data.MySqlClient;
 using System.Data;
+using System.Diagnostics;
 using System.Web;
 using System.Linq;
+using Microsoft.AspNetCore.Mvc;
 using MySql.Data;
 using MySql;
 using MySql.Web;
@@ -11,35 +13,40 @@ namespace Webapp.Pages;
 
 public class LoginPage : PageModel
 {
+
+
+    [BindProperty]
+    public string Message { get; set; }
     
-    protected void ButtonClick(object sender, System.EventArgs e)
+    public void OnPostTestButton()
     {
-            Console.Write("Test");
-            Console.WriteLine("Test");
-            
-            string email = "email";
-            string mycon = "server=localhost:3306; Uid=root; password=123aukiboy123; persistsecurityinfo=True; database=mydb; SslMode= none";
+        Message = "TEST";
+        
+        string email = "email";
+        string mycon = "server=localhost:3306; Uid=root; password=123aukiboy123; persistsecurityinfo=True; database=mydb; SslMode= none";
 
-            MySqlConnection con = new MySqlConnection(mycon);
-            MySqlCommand cmd = null;
-            try
-            {
-                cmd.Parameters.AddWithValue("@a1", email);
-                cmd = new MySqlCommand("insert into account(Username, Email, Password, Rank) values ('Test', 'Test','Test', '1')");
+        MySqlConnection con = new MySqlConnection(mycon);
+        MySqlCommand cmd = null;
+        try
+        {
+            cmd.Parameters.AddWithValue("@a1", email);
+            cmd = new MySqlCommand(
+                "insert into account(Username, Email, Password, Rank) values ('Test', 'Test','Test', '1')");
 
-                
-                con.Open();
-                cmd.ExecuteNonQuery();
-                con.Close();
 
-            }
-            catch (Exception ex)
-            {
-                Response.WriteAsync("<script>alert('" + ex.Message + "')</script>");
-                con.Close();
-                return;
-            }
-                Response.WriteAsync("<script>alert('Data saved successfully')</script>");
+            con.Open();
+            cmd.ExecuteNonQuery();
+            con.Close();
+
         }
-    
+        catch (Exception ex)
+        {
+            Response.WriteAsync("<script>alert('" + ex.Message + "')</script>");
+            con.Close();
+            return;
+        }
+
+        Response.WriteAsync("<script>alert('Data saved successfully')</script>");
+    }
+
 }
