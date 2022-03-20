8dc4c3008e97333840c3697d363cee7080053795
Fris44 <thefrisiangamer@gmail.com>
2022-03-20 20:00:14 +0100

    En weer naar PC

--- Webapp/Webapp/Pages/AddEntry.cshtml.cs
+++ Webapp/Webapp/Pages/AddEntry.cshtml.cs
@@ -1,97 +1,53 @@
-using System.ComponentModel.DataAnnotations;
-using System.IO;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Extensions.Configuration;
 
-namespace Webapp.Pages;
+namespace Webapp;
 
 public class AddEntry : PageModel
 {
-    private readonly long _fileSizeLimit;
-        private readonly string[] _permittedExtensions = { ".txt" };
-        private readonly string _targetFilePath;
-
-        public BufferedSingleFileUploadPhysicalModel(IConfiguration config)
-        {
-            _fileSizeLimit = config.GetValue<long>("FileSizeLimit");
-
-            // To save physical files to a path provided by configuration:
-            _targetFilePath = config.GetValue<string>("StoredFilesPath");
-
-            // To save physical files to the temporary files folder, use:
-            //_targetFilePath = Path.GetTempPath();
-        }
-
-        [BindProperty]
-        public BufferedSingleFileUploadPhysical FileUpload { get; set; }
-
-        public string Result { get; private set; }
-
-        public void OnGet()
-        {
-        }
-
-        public async Task<IActionResult> OnPostUploadAsync()
-        {
-            if (!ModelState.IsValid)
-            {
-                Result = "Please correct the form.";
-
-                return Page();
-            }
-
-            var formFileContent = 
-                await FileHelpers.ProcessFormFile<BufferedSingleFileUploadPhysical>(
-                    FileUpload.FormFile, ModelState, _permittedExtensions, 
-                    _fileSizeLimit);
-
-            if (!ModelState.IsValid)
-            {
-                Result = "Please correct the form.";
-
-                return Page();
-            }
-
-            // For the file name of the uploaded file stored
-            // server-side, use Path.GetRandomFileName to generate a safe
-            // random file name.
-            var trustedFileNameForFileStorage = Path.GetRandomFileName();
-            var filePath = Path.Combine(
-                _targetFilePath, trustedFileNameForFileStorage);
-
-            // **WARNING!**
-            // In the following example, the file is saved without
-            // scanning the file's contents. In most production
-            // scenarios, an anti-virus/anti-malware scanner API
-            // is used on the file before making the file available
-            // for download or for use by other systems. 
-            // For more information, see the topic that accompanies 
-            // this sample.
-
-            using (var fileStream = System.IO.File.Create(filePath))
-            {
-                await fileStream.WriteAsync(formFileContent);
-
-                // To work directly with a FormFile, use the following
-                // instead:
-                //await FileUpload.FormFile.CopyToAsync(fileStream);
-            }
-
-            return RedirectToPage("./Index");
-        }
-    }
-
-    public class BufferedSingleFileUploadPhysical
+    private IWebHostEnvironment Environment;
+    public string Message { get; set; }
+ 
+    public AddEntry(IWebHostEnvironment _environment)
     {
-        [Required]
-        [Display(Name="File")]
-        public IFormFile FormFile { get; set; }
+        Environment = _environment;
+    }
+ 
+    public void OnGet()
+    {
+ 
+    }
+ 
+    public void OnPostUpload(List<IFormFile> postedFiles)
+    {
+        string path = Path.Combine(this.Environment.WebRootPath, "content");
+        if (!Directory.Exists(path))
+        {
+            Directory.CreateDirectory(path);
+        }
+ 
+        List<string> uploadedFiles = new List<string>();
+        foreach (IFormFile postedFile in postedFiles)
+        {
+            string fileName = null;
+            int i = 0;
+            string extension = Path.GetExtension(postedFile.FileName);
+            while (i == 0)
+            {
+                fileName = Path.GetRandomFileName();
+                fileName = Path.ChangeExtension(fileName, extension);
 
-        [Display(Name="Note")]
-        [StringLength(50, MinimumLength = 0)]
-        public string Note { get; set; }
+                if (!System.IO.File.Exists(fileName))
+                {
+                    i++;
+                }
+            }
+
+            using (FileStream stream = new FileStream(Path.Combine(path, fileName), FileMode.Create))
+            {
+                postedFile.CopyTo(stream);
+                uploadedFiles.Add(fileName);
+                this.Message += string.Format("<b>{0}</b> uploaded.<br />", fileName);
+            }
+        }
     }
 }
