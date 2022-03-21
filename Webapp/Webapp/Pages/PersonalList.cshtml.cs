18d7e1e2130927b5f91389a1409207e7d1a4a0ba
Fris44 <thefrisiangamer@gmail.com>
2022-03-21 13:20:36 +0100

    Order List

--- Webapp/Webapp/Pages/PersonalList.cshtml.cs
+++ Webapp/Webapp/Pages/PersonalList.cshtml.cs
@@ -6,8 +6,29 @@ namespace Webapp.Pages;
 public class PersonalList : PageModel
 {
     public List<PersonalLists> List { get; set; }
+
+    private int order = 0;
+    
     public void OnGet()
     {
-        List = new PersonalListRepo().GetPersList();
+        List = new PersonalListRepo().GetPersList(order);
+    }
+
+    public void OnPostNameButton()
+    {
+        order = 0;
+        OnGet();
+    }
+
+    public void OnPostSeriesButton()
+    {
+        order = 1;
+        OnGet();
+    }
+
+    public void OnPostAuthorButton()
+    {
+        order = 2;
+        OnGet();
     }
 }
--- Webapp/Webapp/PersonalListRepo.cs
+++ Webapp/Webapp/PersonalListRepo.cs
@@ -5,17 +5,12 @@ namespace Webapp;
 
 public class PersonalListRepo
 {
-    // public class ListInfo
-    // {
-    //     public string Name { get; set; }
-    //     public string Series { get; set; }
-    //     public string Summary { get; set; }
-    //     public string Author { get; set; }
-    // }
-
-    public List<PersonalLists> GetPersList()
+    public List<PersonalLists> GetPersList(int order)
     {
+        var parameters = new {Order = order};
         using var db = DbUtils.Connect();
-        return db.Query<PersonalLists>("SELECT Name, Series, Summary, Photo_front, Author FROM comics.comic_information").ToList();
+        var sql =
+            "SELECT Name, Series, Summary, Photo_front, Author FROM comics.comic_information ORDER BY (CASE WHEN @Order = 0 THEN Name WHEN @Order = 1 THEN Series WHEN @Order = 2 THEN Author END)";
+        return db.Query<PersonalLists>(sql, parameters).ToList();
     }
 }
